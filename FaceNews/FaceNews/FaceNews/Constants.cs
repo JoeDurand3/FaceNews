b5d5a6f019e2a2b99f32421850d94655d2488dd7
Elijah Cain <elijah.cain1995@hotmail.com>
2017-04-01 09:56:02 -0700

    Added Text Analysis API Functionality

--- FaceNews/FaceNews/FaceNews/Constants.cs
+++ FaceNews/FaceNews/FaceNews/Constants.cs
@@ -11,5 +11,7 @@ namespace FaceNews
         public static string TextAnalyticsAPIKey = "421758dba6144f09a4ebe472f54bc034";
         public static string EmotionAPIKey = "caf093b8b27544ea8b0815d769d638cf";
         public static string BingSearchKey = "b5a9bf01dfea44ac81e18b59a7dbedc6";
+        public static string TextAnalyticsAPIURL = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/";
+        public static string languages = "en"; 
     }
 }
---
+++ FaceNews/FaceNewsDebug/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaceNews;
+
+namespace FaceNewsDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = TextAnalyticsService.Instance.GetTextAnalyticsAsync("1", "test this sentence");
+            Task.WaitAll(test);
+
+            var testResult = test.Result;
+            Console.Write(testResult.documents.Count);
+           // byte[] byteData = Encoding.UTF8.GetBytes("");
+        }
+ 
+    }
+}
